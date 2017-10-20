b64144795e3aa29264fd92bbd26d0adfa8929ac1
Peter Allen <pallen@kieferconsulting.com>
2017-10-19 21:09:03 -0700

    updates to presentation

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# SharePointStaturday2017
-SharePoint PnP PowerShell and what it can do for my SharePoint Environment
+# Share Point Staturday 2017
+## SharePoint PnP PowerShell and what it can do for my SharePoint Environment
+
+**Check what is installed**
+
+Get-Command -Module *PnP*
+
+**SharePoint PowerShell PnP**
+
+**Make sure you have the correct version ofSharePoint PowerShell PnP installed**
+
+SharePoint Online
+* Install-Module SharePointPnPPowerShellOnline
+* Uninstall-Module SharePointPnPPowerShellOnline
+
+SharePoint 2016
+* Install-Module SharePointPnPPowerShell2016
+* Uninstall-Module SharePointPnPPowerShell2016
+
+SharePoint 2013
+* Install-Module SharePointPnPPowerShell2013
+* Uninstall-Module SharePointPnPPowerShell2013
+
+
+**FIRST - Connect to SharePoint Site**
+
+Connect-PnPOnline –Url https://kieferconsultinginc.sharepoint.com/sites/SharePointSaturday2017/
+
+**Second - Create a list**
+
+New-PnPList -Title "First Demo List" -Url "DemoList" -Template Announcements
+
+**Third - Set the List Properties**
+
+Set-PnPList -Identity "First Demo List" -EnableVersioning $true -MajorVersions 20
+
+**Fourth - Add list item**
+
+Add-PnPListItem -List "First Demo List" -ContentType "Item" -Values @{"Title" = "Test Title 1"; "Body" = "Here is content for the body"}
+
+**Fifth - Add Navigation to Quick Launch**
+
+Add-PnPNavigationNode -Title "First Demo List" -Url "https://kieferconsultinginc.sharepoint.com/sites/nicktest/sitetemplatetest/DemoList/" -Location "QuickLaunch"
+
+**Last - Add a View**
+
+Add-PnPView -List "Demo List" -Title "First Demo List Items" -Fields "Title","Body"
+
+**Other Section**
+
+**Set Credentials**
+$MyCredential = Get-Credential
+
+.\addlist.ps1 -Credentials $MyCredential
+
+**Connect with credentials**
+
+Connect-PnPOnline –Url https://kieferconsultinginc.sharepoint.com/sites/SharePointSaturday2017/ -Credentials $MyCredential
+
+Add-PnPField -List "Demo list" -DisplayName "Item Location" -InternalName "SPSLocation" -Type Choice -Group "Demo Group" -AddToDefaultView -Choices "Stockholm","Helsinki","Oslo"
+
+Remove-PnPView -List "Demo List" -Identity "Demo List Items"
+
+Add-PnPView -List "Demo List" -Title "Demo List Items" -Fields "Title","Body","Location"
---
+++ addList.ps1
@@ -0,0 +1,173 @@
+#===================================================================================
+#
+# Project Upload
+#
+#===================================================================================
+
+#===================================================================================
+# Command Parameters
+#===================================================================================
+param(
+    [Parameter(Mandatory = $false, HelpMessage = "Enter the URL of the target site, e.g. 'https://intranet.mydomain.com/sites/targetSite'")]
+    [String]
+    $targetSiteUrl,
+
+    [Parameter(Mandatory = $false, HelpMessage = "Optional administration credentials")]
+    [PSCredential]
+    $Credentials
+)
+
+#===================================================================================
+# Variables
+#===================================================================================
+$spAddSites = @(
+    @{
+        site = "https://kieferconsultinginc.sharepoint.com/sites/nicktest/sitetemplatetest/testfrommodern/"
+    }
+    @{
+        site = "https://kieferconsultinginc.sharepoint.com/sites/nicktest/sitetemplatetest/testfrommodern2/"
+    }
+)
+$spAddLists = @(
+    @{
+        Title    = "Demo List"
+        URL      = "DemoList"
+        Template = "Announcements"
+    }
+    @{
+        Title    = "Demo Contacts"
+        URL      = "DemoContacts"
+        Template = "Contacts"
+    }
+    @{
+        Title    = "Demo Custom"
+        URL      = "DemoCustom"
+        Template = "GenericList"
+    }
+)
+$spAddViews = @(
+    @{ 
+        ListTitle   = "Demo List"
+        ViewTitle   = "New View"
+        ViewFields  = "Title,Body"
+        ViewQuery   = '<OrderBy><FieldRef Name="Title" /></OrderBy>' 
+        DefaultView = "" 
+    }
+    @{ 
+        ListTitle   = "Demo Contacts"
+        ViewTitle   = "New View"
+        ViewFields  = "Edit,LinkTitle,FirstName,JobTitle,CellPhone,WorkPhone,Email"
+        ViewQuery   = '<OrderBy><FieldRef Name="Title" /></OrderBy>' 
+        DefaultView = "yes" 
+    }
+    @{ 
+        ListTitle   = "Demo Custom"
+        ViewTitle   = "New View"
+        ViewFields  = "Title"
+        ViewQuery   = '<OrderBy><FieldRef Name="Title" /></OrderBy>' 
+        DefaultView = "yes" 
+    }
+)
+$spListSettingsUpdate = @(
+    @{ 
+        ListTitle     = "Demo List"
+        MajorVersions = 30
+    }
+    @{ 
+        ListTitle     = "Demo Contacts"
+        MajorVersions = 20
+    }
+    @{ 
+        ListTitle     = "Demo Custom"
+        MajorVersions = 10
+    }
+)
+
+#===================================================================================
+# Verify Credentials
+#===================================================================================
+if ($Credentials -eq $null) {
+    $Credentials = Get-Credential -Message "Enter Admin Credentials"
+}
+
+
+#===================================================================================
+# Add Lists
+#===================================================================================
+
+foreach ($spSite in $spAddSites) {
+    Connect-PnPOnline $spSite.site -Credentials $Credentials
+
+    Write-Host ""
+    Write-Host -ForegroundColor Green "Connected to:" -nonewline;
+    Write-Host -ForegroundColor White $spSite.site -nonewline;
+    Write-Host ""
+
+    
+    #===================================================================================
+    # Add Lists
+    #===================================================================================
+
+    # foreach ($spList in $spAddLists) {
+
+    #     $listTitle = $spList.Title
+    #     $listUrl = $spList.Url
+    #     $listTemplate = $spList.Template
+    
+    #     $newList = New-PnPList -Title "$listTitle" -Url "$listUrl" -Template $listTemplate -EnableVersioning -OnQuickLaunch
+    
+    #     Write-Host -ForegroundColor White "LIST: " -nonewline; 
+    #     Write-Host -ForegroundColor Green $listTitle -nonewline; 
+    #     Write-Host -ForegroundColor White " - ADDED"
+    
+    # }
+        
+    #===================================================================================
+    # Add List View
+    #===================================================================================
+
+    foreach ($spViewsAdd in $spAddViews) {
+
+        $listTitle = $spViewsAdd.ListTitle
+        $viewTitle = $spViewsAdd.ViewTitle
+        $viewFields = $spViewsAdd.ViewFields -split (',')
+        $viewQuery = $spViewsAdd.ViewQuery
+        $viewDefaultView = $spViewsAdd.DefaultView
+
+        Remove-PnPView -List "$listTitle" -Identity "$viewTitle" -Force
+
+        If ( $viewDefaultView -eq 'yes') {
+
+            Add-PnPView -List $listTitle -Title "$viewTitle" -Fields $viewFields -Query "$viewQuery" -SetAsDefault
+
+        }
+        else {
+                                
+            Add-PnPView -List $listTitle -Title "$viewTitle" -Fields $viewFields -Query "$viewQuery"
+
+        }
+                            
+        Write-Host -ForegroundColor White "LIST: " -nonewline; 
+        Write-Host -ForegroundColor Green $listTitle -nonewline; 
+        Write-Host -ForegroundColor White " - VIEW ADDED: " -nonewline; 
+        Write-Host -ForegroundColor Green $viewTitle
+
+    }
+
+    #===================================================================================
+    # Update Lists Settings
+    #===================================================================================
+
+    foreach ($spListSettings in $spListSettingsUpdate) {
+
+        $listTitle = $spListSettings.ListTitle
+        $listMajorVersions = $spListSettings.MajorVersions
+    
+        Set-PnPList -Identity $listTitle -EnableVersioning $true -MajorVersions $listMajorVersions
+    
+        Write-Host -ForegroundColor White "LIST SETTINGS: " -nonewline; 
+        Write-Host -ForegroundColor Green $listTitle -nonewline; 
+        Write-Host -ForegroundColor White " - UPDATED"
+    
+    }
+}
